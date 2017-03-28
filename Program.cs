e0edaad36a9233822d37badb56ab3e2088f137a3
grj1046 <guorenjun1046@126.com>
2017-03-28 18:01:27 +0800

    UseUrls set listen all host

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace cnblogs
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
+                .UseUrls("http://*:5000")
                 .UseStartup<Startup>()
                 .Build();
 
